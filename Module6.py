b41d5f13d3d84f38ce54a0a515b1470c1ec0e0a9
mmcloos <mmcloos@uno.edu>
2020-11-11 16:56:25 -0600

    Module6

---
+++ Module6.py
@@ -0,0 +1,67 @@
+"""
+Maggie Cloos
+Module 6
+11/11/2020
+cloosmm0@gmail.com
+maggie.cloos@portnola.com
+"""
+
+import sys
+import json
+
+with open("input.json","r") as input:
+    customers = json.load(input)
+
+    
+# are all customer numbers unique?
+temp = []
+for value in customers["clients"]:
+    temp.append(value["id"])
+
+
+# add values to set
+unique = set(temp)
+# add values to tuple
+original = temp
+
+
+if len(unique) != len(original):
+    print("There are duplicate ID numbers in the data, exiting!!!")
+    sys.exit()
+else:
+    print("All customer IDs are unique!")
+        
+"""
+1.
+create a set of customer emails & check for uniqueness
+"""
+temp = []
+for value in customers["clients"]:
+    temp.append(value["email"])
+
+emails_unique = set(temp)
+emails_original = temp
+
+if len(emails_unique) != len(emails_original):
+    print("There are duplicate emails! Exiting.")
+    sys.exit()
+
+else:
+    print("All customer emails are unique.")
+
+
+"""
+2.
+create a dictionary of each customer that contains name & email
+write as a JSON file to a new file called email_list.json
+"""
+
+
+"""
+3.
+open the original file & set each male customer isActive to false
+write the new data to a file called current_customers
+"""
+    
+
+
